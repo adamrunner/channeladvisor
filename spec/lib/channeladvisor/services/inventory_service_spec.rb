a13da891c334f708401a4db6276d44e49fb9cf3e
Adam Runner <adamrunner@gmail.com>
2012-09-18 20:48:11 -0700

    added the function to add inventory items to CA

--- spec/lib/channeladvisor/services/inventory_service_spec.rb
+++ spec/lib/channeladvisor/services/inventory_service_spec.rb
@@ -211,6 +211,76 @@ module ChannelAdvisor
           end # with both quantity and price info
         end # with two items
       end # .update_inventory_item_quantity_and_price_list
+
+      describe ".sync_inventory_item" do
+        use_vcr_cassette "responses/inventory_service/sync_inventory_item", :allow_playback_repeats => true
+
+        let(:item) do
+          {
+            :sku => "FAKE001",
+            :title => "Just a fake item",
+            :subtitle => "It's really fake",
+            :short_description => "So fake, doesn't need a description",
+            :description => "It's faker, then the fakest of fakiestest (that word is fake..)",
+            :weight => 15,
+            :supplier_code => nil, #will fail if supplier doesn't exist
+            :warehouse_location => "Isle 13, Shelf 12, Bin 10",
+            :tax_product_code => "OR",
+            :flag_style => "NoFlag",
+            :flag_description => nil, #no flag set,
+            :asin => "123098",
+            :isbn => "65465",
+            :upc => "51919819",
+            :mpn => "980988",
+            :ean => "65461508",
+            :manufacturer => "Fake Products, Inc",
+            :brand => "Fake",
+            :condition => "New",
+            :warranty => "None!",
+            :product_margin => 200,
+            :supplier_po => "ABC-123",
+            :received_in_inventory => DateTime.now,
+            :harmonized_code => "AA",
+            :height => 2,
+            :length => 2,
+            :width => 2,
+            :classification => "Awesome" ,
+            :meta_description => "Blah blah blah",
+            :store_info => {
+              :display_in_store => true,
+              :title => "Just a fake item",
+              :description => "It's faker, then the fakest of fakiestest (that word is fake..)",
+              :category_id => nil #fails if category doesn't exist
+            },
+            :quantity_info => {
+              :update_type  => "Absolute",
+              :total        => 5000
+            },
+            :price_info => {
+              :cost                       => 2.99,
+              :retail_price               => 11.99,
+              :starting_price             => 5.99,
+              :reserve_price              => 7.99,
+              :take_it_price              => 9.99,
+              :second_chance_offer_price  => 8.99,
+              :store_price                => 9.49
+            }
+          }
+        end
+
+        before(:each) do
+          @last_request, @last_response = nil
+          InventoryService.client.config.hooks.define(:sync_inventory_item, :soap_request) do |callback, request|
+            @last_request = request.http
+            @last_response = callback.call
+          end
+        end
+
+        it "returns a SOAP response" do
+          soap_response = InventoryService.sync_inventory_item(item)
+          soap_response.should be_a Savon::SOAP::Response
+        end # returns a SOAP response      
+      end #describe
     end # InventoryService
   end # Services
 end # ChannelAdvisor
